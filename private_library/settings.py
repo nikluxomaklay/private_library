21abfa2eb9c00d05e27a857e7698ffd71d638952
nikluxomaklay <nikluxomaklay@gmail.com>
2025-08-01 12:19:49 +0300

    Configure CSRF trusted origins in settings.py to default to ALLOWED_HOSTS and split into a list.

--- private_library/settings.py
+++ private_library/settings.py
@@ -36,7 +36,8 @@ SECRET_KEY = config['SECRET_KEY']
 DEBUG = config.get('DEBUG', False)
 
 ALLOWED_HOSTS = config['ALLOWED_HOSTS'].split(' ')
-
+CSRF_TRUSTED_ORIGINS = config.get('CSRF_TRUSTED_ORIGINS', config['ALLOWED_HOSTS'])
+CSRF_TRUSTED_ORIGINS = CSRF_TRUSTED_ORIGINS.split(' ')
 
 # Application definition
 
